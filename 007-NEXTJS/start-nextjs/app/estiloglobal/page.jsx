ad834058e9ba6b4715a65468c20059774d1f3e82
leandrorgrosso <leandrorgrosso@gmail.com>
2023-09-18 21:48:13 -0300

    Estilização

---
+++ 007-NEXTJS/start-nextjs/app/estiloglobal/page.jsx
@@ -0,0 +1,17 @@
+export default function Sobre() {
+    return (
+      <>
+        <h1>Hello Texto da pagina Sobre</h1>
+        <h1 className="sobre1">
+          <div className="textoEstilo">#04 Texto escopado </div>
+          <div className="verde">#04 Texto escopado </div>
+          <a href="#">Teste do Link</a>
+        </h1>
+  
+        <div className="vermelho">#01 Texto vermelho </div>
+        <div className="azul">#02 Texto azul </div>
+        <div className="amarelo">#03 Texto amarelo </div>
+      </>
+    );
+  }
+  
