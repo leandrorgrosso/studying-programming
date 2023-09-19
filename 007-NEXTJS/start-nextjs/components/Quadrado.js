eb0ceacce556cb4d5ce826ac32366df5d1dadfa3
leandrorgrosso <leandrorgrosso@gmail.com>
2023-09-18 22:55:20 -0300

    componentes 01

---
+++ 007-NEXTJS/start-nextjs/components/Quadrado.js
@@ -0,0 +1,7 @@
+import Styles from "../components/quadrado.module.css";
+
+export default function Quadrado() {
+    return(
+        <h1 className={Styles.meuQuadrado}>Texto do Componente Quadrado</h1>
+    )
+}
---
+++ 007-NEXTJS/start-nextjs/components/Receita.js
@@ -0,0 +1,19 @@
+
+function Receita(){
+  return (
+    <div>
+
+    <div>Receita</div>
+    <ul>
+        <li>01</li>
+        <li>02</li>
+        <li>03</li>
+        <li>04</li>
+        <li>05</li>
+    </ul>
+
+    </div>
+  )
+}
+
+export default Receita
