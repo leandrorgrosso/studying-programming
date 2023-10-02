cd762eeb221050c8fb5361f7595f537ae64b5388
leandrorgrosso <leandrorgrosso@gmail.com>
2023-10-02 07:32:40 -0300

    aula hora saudação

--- 007-NEXTJS/start-nextjs/app/minhasprops/cartaopropspage/page.jsx
+++ 007-NEXTJS/start-nextjs/app/minhasprops/cartaopropspage/page.jsx
@@ -1,13 +1,26 @@
 import { Cartao } from "@/components/Cartao"
 
-function CartaoPropsPage() {
+let mensagem = ""
+let numero1 = 0
+let numero2 = 0
+let resposta = numero1 + numero2
+
+  if(resposta === 0){
+    mensagem = "Zerou"
+  }
+
+function QualquerCoisa() {
   return (
     <>
       <div>CartaoPropsPage</div>
-      <Cartao textoDaFrase="Algum texto da frase" autorDaFrase="Autor da Frase" />
+      <Cartao textoDaFrase="Fazendo meu filme" autorDaFrase="Paula Pimenta" />
+
+    <h3>{mensagem}</h3>   
+
+    
     </>
 
   )
 }
 
-export default CartaoPropsPage
+export default QualquerCoisa
--- 007-NEXTJS/start-nextjs/app/saudacao/page.jsx
+++ 007-NEXTJS/start-nextjs/app/saudacao/page.jsx
@@ -1,31 +1,16 @@
-import styles from "./saudacao.module.css";
+import Horas from "@/components/hora";
 
-// const horaCompleta = new Intl.DateTimeFormat("pt-BR", {
-//     timeStyle: "short",
-//     hour12: false,
-// }).format();
-const horaCompleta = new Date()
 
-const aHora = new Date().getHours();
-let minhaSaudacao = "";
 
-if (aHora >= 0 && aHora < 12) {
-    minhaSaudacao = "Bom dia";
-} else if (aHora >= 12 && aHora < 18) {
-    minhaSaudacao = "Boa tarde";
-} else if (aHora >= 18 && aHora <= 23) {
-    minhaSaudacao = "Boa noite";
-}
+
+
 
 function Saudacao() {
     return (
         <>
-            <div className={styles.configuracao}>
-                {/* <div className={styles.hora}>00:00</div> */}
-                {/* <div className={styles.textoSaudacao}>Saudação</div> */}
-                <div className={styles.hora}>{horaCompleta}</div>
-                <div className={styles.textoSaudacao}>{minhaSaudacao}</div>
-            </div>
+        <h1>Exercicio de uso de IF com renderização</h1>
+        <Horas></Horas>    
+          
         </>
     );
 }
