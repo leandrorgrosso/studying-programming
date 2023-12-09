6bda58cf8cd86403c0ba5c88f1bda8bee2283f8f
leandrorgrosso <leandrorgrosso@gmail.com>
2023-12-09 17:21:40 -0300

    acerto layout footer

--- 007-NEXTJS/my-app/components/Footer.jsx
+++ 007-NEXTJS/my-app/components/Footer.jsx
@@ -1,9 +1,18 @@
 // Footer.jsx
-import React from 'react';
-import styles from '@/styles/Footer.module.css';
+import React from "react";
+import styles from "@/styles/Footer.module.css";
 
 const Footer = () => {
-  return <footer className={styles.footer}>Footer</footer>;
+  return (
+    // <footer className={styles.footer}>
+    //   <p>&copy; 2023 Seu Site. Todos os direitos reservados.</p>
+    // </footer>
+    <footer className={styles.footer}>
+    <div className={styles.column}>Column 1</div>
+    <div className={styles.column}>Column 2</div>
+    <div className={styles.column}>Column 3</div>
+  </footer>
+  );
 };
 
 export default Footer;
